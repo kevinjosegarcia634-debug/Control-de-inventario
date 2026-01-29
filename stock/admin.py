f8de353ee29d61de1b92ba100673a6c9b2c72154
Kevin <kevinjosegarcia634@gmail.com>
2026-01-28 21:58:39 -0400

    Mi primer inventario

---
+++ stock/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Producto, Movimiento
+
+admin.site.register(Producto)
+admin.site.register(Movimiento)
---
+++ stock/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Producto, Movimiento
+
+def inventario_view(request):
+    # Si el usuario envía el formulario (POST)
+    if request.method == 'POST':
+        prod_id = request.POST.get('producto_id')
+        tipo_mov = request.POST.get('tipo')
+        cant = request.POST.get('cantidad')
+
+        # Buscamos el producto y creamos el movimiento
+        producto_obj = Producto.objects.get(id=prod_id)
+        Movimiento.objects.create(
+            producto=producto_obj,
+            tipo=tipo_mov,
+            cantidad=int(cant)
+        )
+        return redirect('/') # Recarga la página para ver los cambios
+
+    # Si el usuario solo entra a ver la página (GET)
+    todos_los_productos = Producto.objects.all()
+    return render(request, 'index.html', {'productos': todos_los_productos})
