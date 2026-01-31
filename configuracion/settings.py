6e43eb8782190ccdab572e68e7aabcc551406981
Kevin <kevinjosegarcia634@gmail.com>
2026-01-30 21:23:34 -0400

    el logo

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -116,6 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# Solo agrega esto al final, no borres nada de lo que está arriba
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Esto es lo que estamos configurando ahora:
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
